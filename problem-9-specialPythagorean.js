17321c8a9b2b394dc8950a0e1db0ad61df40ee17
James Kim <yameskim@gmail.com>
2018-01-13 21:58:39 -0500

    Add problem 7, 8, and 9

---
+++ problem-9-specialPythagorean.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* Problem 9: Special pythagorean triplet
+ *
+ * A Pythagorean triplet is a set of three natural numbers,
+ * a < b < c, for which, a^2 + b^2 = c^2
+ *
+ * For example, 3^2 + 4^2 = 9 + 6 = 25 = 5^2
+ *
+ * There exists exactly one Pythagorean triplet for which
+ * a + b + c = 1000.
+ * Find the product abc.
+ *
+ * */
+
+function pythagoreanRunner(x) {
+	
+}
+
+pythagoreanRunner(1000);
